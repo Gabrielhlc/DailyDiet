f077c8683affd39b169fd8d6f1002a6e6911ef75
Gabrielhlc <gabrielhenlc@gmail.com>
2023-02-28 10:19:15 -0300

    fix: newMeal ScrollView

--- src/screens/NewMeal/index.tsx
+++ src/screens/NewMeal/index.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { View, Alert } from "react-native";
+import { View, Alert, ScrollView } from "react-native";
 import { useNavigation, useRoute } from "@react-navigation/native";
 
 import * as yup from "yup";
@@ -126,81 +126,83 @@ export function NewMeal() {
     }, [])
 
     return (
-        <Container>
-            <Header title={headerTitle} />
+        <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ flexGrow: 1 }}>
+            <Container>
+                <Header title={headerTitle} />
 
-            <Body>
+                <Body>
 
-                <Input
-                    label="Nome"
-                    numberOfLines={1}
-                    value={values.name}
-                    onChangeText={handleChange('name')}
-                    onBlur={handleBlur('name')}
-                    error={errors.name}
-                    touched={touched.name}
-                />
+                    <Input
+                        label="Nome"
+                        numberOfLines={1}
+                        value={values.name}
+                        onChangeText={handleChange('name')}
+                        onBlur={handleBlur('name')}
+                        error={errors.name}
+                        touched={touched.name}
+                    />
 
-                <Input
-                    label="Descrição"
-                    numberOfLines={5}
-                    multiline
-                    value={values.description}
-                    onChangeText={handleChange('description')}
-                    onBlur={handleBlur('description')}
-                    error={errors.description}
-                    touched={touched.description}
-                />
+                    <Input
+                        label="Descrição"
+                        numberOfLines={5}
+                        multiline
+                        value={values.description}
+                        onChangeText={handleChange('description')}
+                        onBlur={handleBlur('description')}
+                        error={errors.description}
+                        touched={touched.description}
+                    />
 
-                <InputRow>
-                    <View style={{ flex: 1, marginRight: 10 }}>
-                        <Input
-                            label="Data"
-                            numberOfLines={1}
-                            value={values.date}
-                            onChangeText={handleChange("date")}
-                            onBlur={handleBlur('date')}
-                            error={errors.date}
-                            touched={touched.date}
-                        />
-                    </View>
-                    <View style={{ flex: 1, marginLeft: 10 }}>
-                        <Input
-                            label="Hora"
-                            numberOfLines={1}
-                            value={values.time}
-                            onChangeText={handleChange("time")}
-                            onBlur={handleBlur('time')}
-                            error={errors.time}
-                            touched={touched.time}
-                        />
-                    </View>
-                </InputRow>
+                    <InputRow>
+                        <View style={{ flex: 1, marginRight: 10 }}>
+                            <Input
+                                label="Data"
+                                numberOfLines={1}
+                                value={values.date}
+                                onChangeText={handleChange("date")}
+                                onBlur={handleBlur('date')}
+                                error={errors.date}
+                                touched={touched.date}
+                            />
+                        </View>
+                        <View style={{ flex: 1, marginLeft: 10 }}>
+                            <Input
+                                label="Hora"
+                                numberOfLines={1}
+                                value={values.time}
+                                onChangeText={handleChange("time")}
+                                onBlur={handleBlur('time')}
+                                error={errors.time}
+                                touched={touched.time}
+                            />
+                        </View>
+                    </InputRow>
 
-                <RadiosTitle>Está dentro da dieta?</RadiosTitle>
-                <InputRow>
-                    <View style={{ flex: 1, marginRight: 10 }}>
-                        <RadioButton
-                            title="Sim"
-                            isActive={inDiet === 'yes'}
-                            onPress={() => handleIsDiet('yes')}
-                        />
-                    </View>
-                    <View style={{ flex: 1, marginLeft: 10 }}>
-                        <RadioButton
-                            type="SECONDARY"
-                            title="Não"
-                            isActive={inDiet === 'no'}
-                            onPress={() => handleIsDiet('no')}
-                        />
-                    </View>
-                </InputRow>
+                    <RadiosTitle>Está dentro da dieta?</RadiosTitle>
+                    <InputRow>
+                        <View style={{ flex: 1, marginRight: 10 }}>
+                            <RadioButton
+                                title="Sim"
+                                isActive={inDiet === 'yes'}
+                                onPress={() => handleIsDiet('yes')}
+                            />
+                        </View>
+                        <View style={{ flex: 1, marginLeft: 10 }}>
+                            <RadioButton
+                                type="SECONDARY"
+                                title="Não"
+                                isActive={inDiet === 'no'}
+                                onPress={() => handleIsDiet('no')}
+                            />
+                        </View>
+                    </InputRow>
 
-                <ButtonContainer>
-                    <Button disabled={!isValid || inDiet == undefined} title={buttonTitle} onPress={() => handleSubmit()} />
-                </ButtonContainer>
+                    <ButtonContainer>
+                        <Button disabled={!isValid || inDiet == undefined} title={buttonTitle} onPress={() => handleSubmit()} />
+                    </ButtonContainer>
 
-            </Body>
-        </Container>
+                </Body>
+            </Container>
+        </ScrollView>
     )
 }
